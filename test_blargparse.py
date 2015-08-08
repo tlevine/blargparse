6eb4ad7e0f619cebc63b473c266d321cd71dcf01
Thomas Levine <_@thomaslevine.com>
2015-08-08 11:41:31 -0400

    start tests

---
+++ test_blargparse.py
@@ -0,0 +1,7 @@
+import blargparser
+
+@pytest.mark.parametrize('blargparser, dest, func, expected_aggregates', testcases_aggregates)
+def test_add_aggregate(blargparser, dest, func, expected_aggregates):
+
+@pytest.mark.parametrize('blargparser, dest, func, expected_namespace', testcases_namespace)
+def test_parse_args(blargparser, dest, func, expected_namespace)
