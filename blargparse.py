f53b9463c40f93cd78bb44265a875c0f7822669d
Thomas Levine <_@thomaslevine.com>
2015-08-08 11:37:08 -0400

    add_aggregate

--- blargparse.py
+++ blargparse.py
@@ -1,3 +1,18 @@
 import argparse
 
 class BlargParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super(BlargParser, self).__init__(*args, **kwargs)
+        self._aggregates = {}
+
+    def add_aggregate(self, dest, func = lambda args:None):
+        if dest in self._aggregates:
+            raise argparse.ArgumentError('aggregate %s already exists')
+        else:
+            self._aggregates[dest] = func
+
+    def parse_args(self, *args, **kwargs):
+        namespace = super(BlargParser, self).parse_args(*args, **kwargs)
+        for dest, func in self._aggregates.items():
+            setattr(namespace, dest, func(namespace))
+        return namespace
