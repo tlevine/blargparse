12f1e694b792db6c01973b1378013e417d9b2a0a
Thomas Levine <_@thomaslevine.com>
2015-08-08 12:38:29 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='blargparse',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Combine arguments inside your blargument parser.',
+      url='https://github.com/tlevine/blargparse',
+      py_modules=['blargparse'],
+      tests_require = ['pytest'],
+      version='0.0.1',
+      license='LGPL',
+)
+
